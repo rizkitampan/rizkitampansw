90115f7c9d6ef8001793b44b5317b724eec4ff1b
RizkiTampan <rizkitampan@users.noreply.github.com>
2019-07-11 08:55:27 +0700

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,6 @@
+self.addEventListener('install', (event) => {
+	console.log('service worker installed', event);
+});
+self.addEventListener('activate', (event) => {
+	console.log('service worker activated', event);
+});
