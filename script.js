52f227f3be298e8ca15950fa054a052caa3c1755
RizkiTampan <rizkitampan@users.noreply.github.com>
2019-07-11 09:08:25 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,15 @@
+(() => {
+	if ('serviceWorker' in navigator) {
+		window.addEventListener('load', () => {
+			navigator.serviceWorker.register('service-worker.js').
+			then((registration) => {
+				console.log('registered');
+				console.log(registration);
+			},(err) => {
+				console.log(err);
+			});
+		});
+	} else {
+		alert('No service worker support in this browser');
+	}
+});
