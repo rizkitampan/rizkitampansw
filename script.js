0437c728f5c5f1a466779f44f1556974a6ca27a6
RizkiTampan <rizkitampan@users.noreply.github.com>
2019-07-11 15:19:41 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+// ServiceWorker is a progressive technology. Ignore unsupported browsers
 if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('service-worker.js');
+  console.log('CLIENT: service worker registration in progress.');
+  navigator.serviceWorker.register('/service-worker.js').then(function() {
+    console.log('CLIENT: service worker registration complete.');
+  }, function() {
+    console.log('CLIENT: service worker registration failure.');
+  });
+} else {
+  console.log('CLIENT: service worker is not supported.');
 }
