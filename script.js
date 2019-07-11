23fcba036190d787931b525f0105de1012808172
RizkiTampan <rizkitampan@users.noreply.github.com>
2019-07-11 15:16:16 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,3 @@
-	if ('serviceWorker' in navigator) {
-		window.addEventListener('load', () => {
-			navigator.serviceWorker.register('service-worker.js').then((registration) => {
-				console.log('registered');
-				console.log(registration);
-			},(err) => {
-				console.log(err);
-			});
-		});
-	} else {
-		alert('No service worker support in this browser');
-	}
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('service-worker.js');
+}
